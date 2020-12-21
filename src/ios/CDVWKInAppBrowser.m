f762f198938999ffa579a8e776164eae4edea6bf
Grant Hiebert <granthiebert@grants-air.lan>
2020-12-20 21:36:10 -0700

    Soft Close

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -655,7 +655,39 @@ static CDVWKInAppBrowser* instance = nil;
 
 - (void)browserSoftExit
 {
-    [self browserExit];
+     if (self.callbackId != nil) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                      messageAsDictionary:@{@"type":@"exit"}];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+        self.callbackId = nil;
+    }
+    
+    [self.inAppBrowserViewController.configuration.userContentController removeScriptMessageHandlerForName:IAB_BRIDGE_NAME];
+    self.inAppBrowserViewController.configuration = nil;
+    
+    [self.inAppBrowserViewController.webView stopLoading];
+    [self.inAppBrowserViewController.webView removeFromSuperview];
+    [self.inAppBrowserViewController.webView setUIDelegate:nil];
+    [self.inAppBrowserViewController.webView setNavigationDelegate:nil];
+    self.inAppBrowserViewController.webView = nil;
+    
+    // Set navigationDelegate to nil to ensure no callbacks are received from it.
+    self.inAppBrowserViewController.navigationDelegate = nil;
+    self.inAppBrowserViewController = nil;
+
+    // Set tmpWindow to hidden to make main webview responsive to touch again
+    // Based on https://stackoverflow.com/questions/4544489/how-to-remove-a-uiwindow
+    self->tmpWindow.hidden = YES;
+    self->tmpWindow = nil;
+
+    if (IsAtLeastiOSVersion(@"7.0")) {
+        if (_previousStatusBarStyle != -1) {
+            [[UIApplication sharedApplication] setStatusBarStyle:_previousStatusBarStyle];
+            
+        }
+    }
+    
+    _previousStatusBarStyle = -1; // this
     // if (self.callbackId != nil) {
     //    	NSString* url = [self.inAppBrowserViewController.currentURL absoluteString];
        	
@@ -675,32 +707,32 @@ static CDVWKInAppBrowser* instance = nil;
         self.callbackId = nil;
     }
     
-    // [self.inAppBrowserViewController.configuration.userContentController removeScriptMessageHandlerForName:IAB_BRIDGE_NAME];
-    // self.inAppBrowserViewController.configuration = nil;
+    [self.inAppBrowserViewController.configuration.userContentController removeScriptMessageHandlerForName:IAB_BRIDGE_NAME];
+    self.inAppBrowserViewController.configuration = nil;
     
-    // [self.inAppBrowserViewController.webView stopLoading];
-    // [self.inAppBrowserViewController.webView removeFromSuperview];
-    // [self.inAppBrowserViewController.webView setUIDelegate:nil];
-    // [self.inAppBrowserViewController.webView setNavigationDelegate:nil];
-    // self.inAppBrowserViewController.webView = nil;
+    [self.inAppBrowserViewController.webView stopLoading];
+    [self.inAppBrowserViewController.webView removeFromSuperview];
+    [self.inAppBrowserViewController.webView setUIDelegate:nil];
+    [self.inAppBrowserViewController.webView setNavigationDelegate:nil];
+    self.inAppBrowserViewController.webView = nil;
     
-    // // Set navigationDelegate to nil to ensure no callbacks are received from it.
-    // self.inAppBrowserViewController.navigationDelegate = nil;
-    // self.inAppBrowserViewController = nil;
+    // Set navigationDelegate to nil to ensure no callbacks are received from it.
+    self.inAppBrowserViewController.navigationDelegate = nil;
+    self.inAppBrowserViewController = nil;
 
-    // // Set tmpWindow to hidden to make main webview responsive to touch again
-    // // Based on https://stackoverflow.com/questions/4544489/how-to-remove-a-uiwindow
-    // self->tmpWindow.hidden = YES;
-    // self->tmpWindow = nil;
+    // Set tmpWindow to hidden to make main webview responsive to touch again
+    // Based on https://stackoverflow.com/questions/4544489/how-to-remove-a-uiwindow
+    self->tmpWindow.hidden = YES;
+    self->tmpWindow = nil;
 
-    // if (IsAtLeastiOSVersion(@"7.0")) {
-    //     if (_previousStatusBarStyle != -1) {
-    //         [[UIApplication sharedApplication] setStatusBarStyle:_previousStatusBarStyle];
+    if (IsAtLeastiOSVersion(@"7.0")) {
+        if (_previousStatusBarStyle != -1) {
+            [[UIApplication sharedApplication] setStatusBarStyle:_previousStatusBarStyle];
             
-    //     }
-    // }
+        }
+    }
     
-    // _previousStatusBarStyle = -1; // this value was reset before reapplying it. caused statusbar to stay black on ios7
+    _previousStatusBarStyle = -1; // this value was reset before reapplying it. caused statusbar to stay black on ios7
 }
 
 @end //CDVWKInAppBrowser
